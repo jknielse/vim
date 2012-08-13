ce26f27387adb2d109b898a3734e65308fbb9b7f
jake <jake@jake-Satellite-L450.(none)>
2012-08-12 21:56:38 -0400

    Adds prerequisites for the global install script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,43 @@
-cp -f ./.vimrc ~/
+#!/bin/bash
+#setup.sh - this is a default setup script to make the process of making these
+#           for other applications easier.
+
+. ~/.libs/colours
+
+function setup {
+    bldylw
+    echo "    Copying the vimrc file to ~/"
+    cp ./.vimrc ~/
+    bldgrn
+    echo "    Done"
+
+    #after everything else, this is probably the last step.
+    bldylw
+    echo "    Updating installation status"
+    echo 0 > ./installation_status
+    bldgrn
+    echo "    Done"
+    bldgrn
+    echo "Installation Successful"
+}
+
+
+bldylw
+echo -n "Installing "
+bldblu
+cat ./program_name
+if [[ -e ./installation_status ]]
+then
+    if [[ `cat ./installation_status` == "0" ]]
+    then
+        bldylw
+        echo "    Program aready installed"
+        bldgrn
+        echo "Installation Successful"
+    else
+        setup
+    fi
+else
+    setup
+fi
+txtrst
